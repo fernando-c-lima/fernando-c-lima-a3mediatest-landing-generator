25a9834bf7f7c729abc8483a60c6f92fb7060e7d
fernando-c-lima <fernandox.caetano@gmail.com>
2025-05-04 21:50:25 -0300

    feat: adicionar collections Page e Blog ao esquema do TinaCMS

--- tina/config.ts
+++ tina/config.ts
@@ -32,6 +32,8 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
+      PageCollection,
+      BlogCollection,
       GlobalConfigCollection,
       Template1Collection, // Atualize para Template1Collection
       Template2Collection, // Atualize para Template2Collection
